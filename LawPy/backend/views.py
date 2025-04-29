9f4349ac74302689acd5e960f27d675d5d573b6b
okuehnd <kuehno98@gmail.com>
2025-04-29 11:53:11 -0400

    Django set up :)

---
+++ LawPy/backend/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+# from .models import 
+# from .serializers import 
+
+# Create your views here.
+# class..(APIView):
+#     def get(self,request):
+#         #serialize response
+
+#     def post(self,request):
+#         #serialize requeset
